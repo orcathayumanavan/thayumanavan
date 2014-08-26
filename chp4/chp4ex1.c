5e9a8f440d6b94fa7530077e07d93c99c456d027
thayumanavan <thayu13@gmail.com>
2014-08-26 14:17:16 +0530

    chapter 4

---
+++ chp4/chp4ex1.c
@@ -0,0 +1,15 @@
+//program to calculate the eighth triangular number
+
+#include<stdio.h>
+
+int main(void0
+{
+	int triangularnumber;
+ 
+	triangularnumber = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8;
+	
+	printf('the eighth triangular number is %i\n",triangularnumber);
+
+	return 0;
+}
+	 
