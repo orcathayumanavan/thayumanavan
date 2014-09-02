76d12bea8f7cf5fdf6f97abb643bdfa634956658
orcatthayumanavan <thayu13@gmail.com>
2014-09-02 13:00:37 -0700

    chapter 5&6

---
+++ chp5/chp5ex9.c
@@ -0,0 +1,26 @@
+/* chapter 5 example 9
+ * thayumanavan , august 2014
+// Program to reverse the digits of a number
+
+#include<stdio.h>
+int main()
+{
+
+	int number, right_digit;
+
+	printf("Enter your number.\n");
+	scanf("%i", &number);
+
+	do 
+{
+	right_digit = number % 10;
+	printf("%i", right_digit);
+	number = number / 10;
+}
+
+	while( number != 0 );
+
+	printf("\n");
+
+	return 0;
+}
---
+++ chp7/chp7ex2.c
@@ -0,0 +1,32 @@
+/* chapter 7 example 2
+ * thayumanavan , august 2014
+*/ demonstrating an array of counters
+
+#include<stdio.h>
+
+int main(void)
+{
+	int ratingCounters[11], i, response;
+
+	for( i = 1; i <= 10; ++i )
+		ratingCounters[i] = 0;
+
+	printf("Enter your responses\n");
+
+	for( i = 1; i <= 20; ++i ) {
+		scanf("%i", &response);
+
+		if( response < 1 || response > 10 )
+			printf("Bad response: %i\n", response);
+		else
+			++ratingCounters[response];
+}
+
+	printf ("\n\nRating Number of Responses\n");
+	printf ("------ -------------------\n");
+
+	for( i = 1; i <= 10; ++i )
+		printf("%4i%14i\n", i, ratingCounters[i]);
+
+return 0;
+}
