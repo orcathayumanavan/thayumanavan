4bf1ab8b17543d377bd21584b779f73892bedc6c
orcatthayumanavan <thayu13@gmail.com>
2014-09-02 13:45:32 -0700

    chap 5&6

---
+++ chp5/chp5ex1.c
@@ -0,0 +1,17 @@
+/* chapter 5 example 1
+ * thayumanavan , august 2014
+// Program to calculate the eighth triangular number*/
+
+#include<stdio.h>
+
+int main(void)
+{
+	int triangularNumber;
+	
+	triangularNumber = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8;
+
+	printf("The eighth triangular number is %i\n",
+	               triangularNumber);
+
+	return 0;
+}
---
+++ chp5/chp5ex6.c
@@ -0,0 +1,17 @@
+/* chapter 5 example 6
+ * thayumanavan , august 2014
+// Program to introduce the while statement*/
+
+#include<stdio.h>
+
+int main(void)
+{
+	int count = 1;
+	while( count <= 5 )
+{
+	printf("%i\n", count);
+	++count;
+}
+
+	return 0;
+}
